d126507d70b6d1ae76edb23ece4cc5a367b5519a
sahildeepmehra <sahil.mehra@guidingstardigital.com>
2019-07-07 03:21:09 +0530

    filter and sort

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,9 @@ export class HomeComponent implements OnInit {
   films = [];
   allFilms;
   page_no = 1;
+  searchKeyword;
+  order = false;
+  orderedBy;
   constructor(private webService: WebService) { }
 
   ngOnInit() {
@@ -20,6 +23,7 @@ export class HomeComponent implements OnInit {
     this.webService.getFilmListing().subscribe(data => {
       if (data) {
         this.allFilms = data;
+        localStorage.setItem('all_films', JSON.stringify(this.allFilms));
         this.getNextSetOfData(this.page_no);
 
       }
@@ -33,9 +37,39 @@ export class HomeComponent implements OnInit {
       });
   }
   onScroll() {
-    console.log('scrolled');
-    this.page_no++;
-    this.getNextSetOfData(this.page_no);
+    if (this.page_no) {
+      this.page_no++;
+      this.getNextSetOfData(this.page_no);
+    }
+  }
+
+  search() {
+    this.allFilms = JSON.parse(localStorage.getItem('all_films'));
+    if (this.searchKeyword !== '') {
+      this.films  = this.allFilms.filter(res => {
+          return res.title.toLowerCase().match(this.searchKeyword.toLowerCase()) || res.director.toLowerCase().match(this.searchKeyword.toLowerCase()) || res.producer.toLowerCase().match(this.searchKeyword.toLowerCase()) ;
+      });
+    } else if (this.searchKeyword === '') {
+        this.page_no = 1;
+        this.films = [];
+        this.getFilmListing();
+    }
+  }
+  sortBy(key) {
+    this.searchKeyword = ''
+    this.page_no = 0;
+    this.order = !this.order;
+    this.orderedBy = key;
+    this.allFilms = JSON.parse(localStorage.getItem('all_films'));
+    if (this.order){
+      this.films = this.allFilms.sort((a, b) => b[key] - a[key]);
+    } else {
+      this.films = this.allFilms.sort((a, b) => a[key] - b[key]);
+
+    }
+
+
+
   }
 
 }
